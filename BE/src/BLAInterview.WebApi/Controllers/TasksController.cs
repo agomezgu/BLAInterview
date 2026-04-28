579379171b6b3b67c8e222999686c83dc1ae6656
andresgomezGL <72281620+andresgomezGL@users.noreply.github.com>
2026-04-28 12:48:29 -0500

    Add Post controller to fix Tests

--- BE/src/BLAInterview.WebApi/Controllers/TasksController.cs
+++ BE/src/BLAInterview.WebApi/Controllers/TasksController.cs
@@ -17,5 +17,15 @@ public sealed class TasksController : ControllerBase
         return Ok(new TaskListResponse("Claim must Be Added", Array.Empty<object>()));
     }
 
+    [HttpPost]
+    public IActionResult CreateTask([FromBody] CreateTaskRequest request, CancellationToken cancellationToken)
+    {
+        return Created("/tasks", new TaskCreationResponse(request.Title));
+    }
+
     private sealed record TaskListResponse(string? UserId, object[] Tasks);
+
+    public sealed record CreateTaskRequest(string Title);
+
+    public sealed record TaskCreationResponse(string Title);
 }
