f4449263e6035f896f6bd6ce661958fe1ea27ba4
andresgomezGL <72281620+andresgomezGL@users.noreply.github.com>
2026-04-28 12:18:46 -0500

    Add red staatus test for Create Task Feature

---
+++ BE/tests/BLAInterview.WebApi.UnitTests/Tasks/TaskCreationEndpointSpecs.cs
@@ -0,0 +1,28 @@
+namespace BLAInterview.WebApi.UnitTests.Tasks;
+
+public class TaskCreationEndpointSpecs
+{
+    [Fact]
+    public void TaskCreationEndpoint_CreatesTask_WhenAuthenticatedUserSubmitsTitle()
+    {
+        Assert.Fail("RED: BE-API-003-T001 not implemented yet.");
+    }
+
+    [Fact]
+    public void TaskCreationEndpoint_AssociatesTaskWithAuthenticatedUser_WhenTaskIsStored()
+    {
+        Assert.Fail("RED: BE-API-003-T002 not implemented yet.");
+    }
+
+    [Fact]
+    public void TaskCreationEndpoint_ReturnsValidationFailure_WhenTitleIsMissing()
+    {
+        Assert.Fail("RED: BE-API-003-T003 not implemented yet.");
+    }
+
+    [Fact]
+    public void TaskCreationEndpoint_ReturnsCreatedTaskDetails_WhenCreationSucceeds()
+    {
+        Assert.Fail("RED: BE-API-003-T004 not implemented yet.");
+    }
+}
