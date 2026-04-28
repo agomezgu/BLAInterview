52a4fb4356e1cd7d38139f29f78b0499e0ef48c8
andresgomezGL <72281620+andresgomezGL@users.noreply.github.com>
2026-04-28 16:42:11 -0500

    Inproving code to have green tests

--- BE/src/BLAInterview.Domain/Tasks/Task.cs
+++ BE/src/BLAInterview.Domain/Tasks/Task.cs
@@ -1,19 +1,17 @@
+using BLAInterview.Domain.Common;
+
 namespace BLAInterview.Domain.Tasks;
 
-public sealed class Task
+public sealed class Task : BaseAuditableEntity
 {
     private Task(string title, string ownerId)
     {
         Title = title;
-        OwnerId = ownerId;
+        Created = DateTimeOffset.UtcNow;
+        CreatedBy = ownerId;
     }
 
     public string Title { get; }
 
-    public string OwnerId { get; }
-
-    public static Task Create(string title, string ownerId)
-    {
-        return new Task(title, ownerId);
-    }
+   
 }
