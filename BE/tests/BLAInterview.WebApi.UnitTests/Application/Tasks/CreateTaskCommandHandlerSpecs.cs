cb5c6ee3a8cbb65ba807fe2a412263c5286b03fc
andresgomezGL <72281620+andresgomezGL@users.noreply.github.com>
2026-04-28 12:57:15 -0500

    Created Tests for Application objects required

---
+++ BE/tests/BLAInterview.WebApi.UnitTests/Application/Tasks/CreateTaskCommandHandlerSpecs.cs
@@ -0,0 +1,28 @@
+namespace BLAInterview.WebApi.UnitTests.Application.Tasks;
+
+public class CreateTaskCommandHandlerSpecs
+{
+    [Fact]
+    public void CreateTaskCommandHandler_CreatesTask_WhenCommandHasTitleAndOwner()
+    {
+        Assert.Fail("RED: BE-API-003-T001 not implemented yet.");
+    }
+
+    [Fact]
+    public void CreateTaskCommandHandler_AssignsOwnerId_WhenTaskIsStored()
+    {
+        Assert.Fail("RED: BE-API-003-T002 not implemented yet.");
+    }
+
+    [Fact]
+    public void CreateTaskCommandHandler_ReturnsValidationFailure_WhenTitleIsMissing()
+    {
+        Assert.Fail("RED: BE-API-003-T003 not implemented yet.");
+    }
+
+    [Fact]
+    public void CreateTaskCommandHandler_ReturnsCreatedTaskDetails_WhenTaskIsCreated()
+    {
+        Assert.Fail("RED: BE-API-003-T004 not implemented yet.");
+    }
+}
