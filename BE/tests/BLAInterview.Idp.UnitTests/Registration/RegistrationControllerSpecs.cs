2532c869cdfbcd442bd0f1d52bd63a5f2dafafcc
andresgomezGL <72281620+andresgomezGL@users.noreply.github.com>
2026-04-27 21:47:07 -0500

    Add Initial Test List for RegistrationController

---
+++ BE/tests/BLAInterview.Idp.UnitTests/Registration/RegistrationControllerSpecs.cs
@@ -0,0 +1,34 @@
+namespace BLAInterview.Idp.UnitTests.Registration;
+
+public class RegistrationControllerSpecs
+{
+    [Fact]
+    public void RegistrationController_ReturnsValidationFailure_WhenNameIsMissing()
+    {
+        Assert.Fail("RED: BE-IDP-001-T001 not implemented yet.");
+    }
+
+    [Fact]
+    public void RegistrationController_ReturnsValidationFailure_WhenEmailIsMissing()
+    {
+        Assert.Fail("RED: BE-IDP-001-T002 not implemented yet.");
+    }
+
+    [Fact]
+    public void RegistrationController_ReturnsValidationFailure_WhenEmailFormatIsInvalid()
+    {
+        Assert.Fail("RED: BE-IDP-001-T003 not implemented yet.");
+    }
+
+    [Fact]
+    public void RegistrationController_ReturnsValidationFailure_WhenPasswordIsMissing()
+    {
+        Assert.Fail("RED: BE-IDP-001-T004 not implemented yet.");
+    }
+
+    [Fact]
+    public void RegistrationController_AcceptsRequest_WhenRegistrationDataIsCompleteAndUsable()
+    {
+        Assert.Fail("RED: BE-IDP-001-T005 not implemented yet.");
+    }
+}
