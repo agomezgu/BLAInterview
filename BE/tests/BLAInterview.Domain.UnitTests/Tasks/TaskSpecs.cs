d08a46813747c56f28ddcaf64901437a87796624
andresgomezGL <72281620+andresgomezGL@users.noreply.github.com>
2026-04-28 16:14:24 -0500

    Adding Domain Tests Specs Red Status

---
+++ BE/tests/BLAInterview.Domain.UnitTests/Tasks/TaskSpecs.cs
@@ -0,0 +1,28 @@
+namespace BLAInterview.Domain.UnitTests.Tasks;
+
+public class TaskSpecs
+{
+    [Fact]
+    public void Task_CreatesTask_WhenTitleAndOwnerAreProvided()
+    {
+        Assert.Fail("RED: BE-API-003-T001 not implemented yet.");
+    }
+
+    [Fact]
+    public void Task_AssignsOwnerId_WhenTaskIsCreated()
+    {
+        Assert.Fail("RED: BE-API-003-T002 not implemented yet.");
+    }
+
+    [Fact]
+    public void Task_RejectsCreation_WhenTitleIsMissing()
+    {
+        Assert.Fail("RED: BE-API-003-T003 not implemented yet.");
+    }
+
+    [Fact]
+    public void Task_ExposesCreatedTaskDetails_WhenTaskIsCreated()
+    {
+        Assert.Fail("RED: BE-API-003-T004 not implemented yet.");
+    }
+}
