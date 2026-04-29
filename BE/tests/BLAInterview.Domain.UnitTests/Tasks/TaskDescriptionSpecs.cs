3067c87754727c73b79fa190c3a12f0cc7508fc7
andresgomezGL <72281620+andresgomezGL@users.noreply.github.com>
2026-04-29 14:15:31 -0500

    Add TaskDecription Specs body code

--- BE/tests/BLAInterview.Domain.UnitTests/Tasks/TaskDescriptionSpecs.cs
+++ BE/tests/BLAInterview.Domain.UnitTests/Tasks/TaskDescriptionSpecs.cs
@@ -5,18 +5,58 @@ public class TaskDescriptionSpecs
     [Fact]
     public void TaskDescription_CreatesDescription_WhenLengthIsBetweenMinimumAndMaximum()
     {
-        Assert.Fail("RED: BE-API-004-A-T001 not implemented yet.");
+        // Arrange
+        var description = "Eleven ok.";
+
+        // Act
+        var taskDescription = BLAInterview.Domain.Tasks.TaskDescription.Create(description);
+
+        // Assert
+        Assert.Equal(description, taskDescription.Value);
     }
 
     [Fact]
     public void TaskDescription_RejectsDescription_WhenLengthIsTenCharactersOrLess()
     {
-        Assert.Fail("RED: BE-API-004-A-T002 not implemented yet.");
+        // Arrange
+        var descriptions = new[]
+        {
+            "",
+            "Short",
+            "1234567890"
+        };
+
+        // Act
+        var createDescriptions = descriptions
+            .Select<Action>(description => () => BLAInterview.Domain.Tasks.TaskDescription.Create(description))
+            .ToArray();
+
+        // Assert
+        foreach (var createDescription in createDescriptions)
+        {
+            Assert.Throws<ArgumentException>(createDescription);
+        }
     }
 
     [Fact]
     public void TaskDescription_RejectsDescription_WhenLengthIsFiftyCharactersOrMore()
     {
-        Assert.Fail("RED: BE-API-004-A-T003 not implemented yet.");
+        // Arrange
+        var descriptions = new[]
+        {
+            new string('A', 50),
+            new string('A', 51)
+        };
+
+        // Act
+        var createDescriptions = descriptions
+            .Select<Action>(description => () => BLAInterview.Domain.Tasks.TaskDescription.Create(description))
+            .ToArray();
+
+        // Assert
+        foreach (var createDescription in createDescriptions)
+        {
+            Assert.Throws<ArgumentException>(createDescription);
+        }
     }
 }
