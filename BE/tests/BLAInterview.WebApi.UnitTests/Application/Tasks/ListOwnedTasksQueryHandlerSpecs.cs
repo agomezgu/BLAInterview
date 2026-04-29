57b5a1d293d4b42bee557fb7fc2ade9032508843
andresgomezGL <72281620+andresgomezGL@users.noreply.github.com>
2026-04-29 10:17:53 -0500

    Add red status Tests for Application layer BE-API-004

---
+++ BE/tests/BLAInterview.WebApi.UnitTests/Application/Tasks/ListOwnedTasksQueryHandlerSpecs.cs
@@ -0,0 +1,22 @@
+namespace BLAInterview.WebApi.UnitTests.Application.Tasks;
+
+public class ListOwnedTasksQueryHandlerSpecs
+{
+    [Fact]
+    public void ListOwnedTasksQueryHandler_ReturnsOwnedTasks_WhenUserHasTasks()
+    {
+        Assert.Fail("RED: BE-API-004-T001 not implemented yet.");
+    }
+
+    [Fact]
+    public void ListOwnedTasksQueryHandler_ReturnsEmptyList_WhenUserHasNoTasks()
+    {
+        Assert.Fail("RED: BE-API-004-T002 not implemented yet.");
+    }
+
+    [Fact]
+    public void ListOwnedTasksQueryHandler_ExcludesOtherUsersTasks_WhenUserRequestsList()
+    {
+        Assert.Fail("RED: BE-API-004-T003 not implemented yet.");
+    }
+}
